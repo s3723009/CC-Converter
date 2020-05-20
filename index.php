d9821f12143d97c06c7f53f576f0529a317712eb
Ming Guan <s3723009@student.rmit.edu.au>
2020-05-21 03:04:16 +1000

    update

--- index.php
+++ index.php
@@ -19,12 +19,16 @@ use google\appengine\api\cloud_storage\CloudStorageTools;
 <body>
     <h1>Cloud Converter</h1>
     <h3>Created by: <br> Ming s3723009 <br> Ty s3668469</h3>
-    <h4>Simple tool to convert jpeg to png and vice-versa!</h4>
+    <h4>Simple tool to convert jpg to png and vice-versa!</h4>
     <div class='userUpload'>
         <form action="upload" enctype="multipart/form-data" method="post">
             <h2>File to upload: <br></h2>
             <input type="file" name="userfile" accept=".jpg, .png" size="40" required= "required">
             <input type="submit" value="Convert">
         </form>
+    </div>
+<div class="disclaimer">
+        <h2>Disclaimer</h2>
+        <p>All files will be deleted at the start of each week. We are not responsible for lost files.</p>
     </div>
 </body>
--- upload_handler.php
+++ upload_handler.php
@@ -7,24 +7,62 @@ if (empty($_FILES['userfile'])){
 header('Location: https://cc-converter-s3723009.ts.r.appspot.com/');
 }  
 
-if($_FILES['userfile']['type'] != 'image/png') {
+// convert to png
+if($_FILES['userfile']['type'] == 'image/jpeg') {
     $date = new DateTime();
     $timeStamp = $date->getTimestamp();
-    $my_bucket = 'cc-converter-s3723009.appspot.com';
-    $file_name = basename($_FILES['userfile']['name']);
-    $temp_name = $_FILES['userfile']['tmp_name'].$timeStamp;
+    $my_bucket = 'cc-converter-storage-png';
+    $file_name = basename($_FILES['userfile']['name'],".jpg");
+    $temp_name = $_FILES['userfile']['tmp_name'];
     move_uploaded_file($temp_name, "gs://${my_bucket}/${file_name}.$timeStamp");
     $public_url = "http://storage.googleapis.com/${my_bucket}/${file_name}.$timeStamp";
+
+echo '<script type="text/javascript">  
+    var timeleft = 60;
+    var downloadTimer = setInterval(function(){
+if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Your Download link is ready";
+    document.getElementById("download").disabled = false;
+} else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+}
+  timeleft -= 1;
+}, 1000);
+   </script>';
 }
 
-if($_FILES['userfile']['type'] != 'image/jpeg') {
+// convert to jpeg
+if($_FILES['userfile']['type'] == 'image/png') {
     $date = new DateTime();
     $timeStamp = $date->getTimestamp();
-    $my_bucket = 'cc-converter-s3723009.appspot.com';
-    $file_name = basename($_FILES['userfile']['name']);
-    $temp_name = $_FILES['userfile']['tmp_name'].$timeStamp;
+    $my_bucket = 'cc-converter-storage-jpeg';
+    $file_name = basename($_FILES['userfile']['name'],".png");
+    $temp_name = $_FILES['userfile']['tmp_name'];
     move_uploaded_file($temp_name, "gs://${my_bucket}/${file_name}.$timeStamp");
     $public_url = "http://storage.googleapis.com/${my_bucket}/${file_name}.$timeStamp";
+    
+echo '<script type="text/javascript">  
+    var timeleft = 60;
+    var downloadTimer = setInterval(function(){
+if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Your Download link is ready";
+    document.getElementById("download").disabled = false;
+} else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+}
+  timeleft -= 1;
+}, 1000);
+   </script>';
+} 
+
+// if unsupported
+if($_FILES['userfile']['type'] != 'image/png' && $_FILES['userfile']['type'] != 'image/jpeg') {
+    echo '<script type="text/JavaScript">  
+     alert("This application only accepts jpg and png files. You will be redirected"); 
+     </script>'; 
+    echo "<script> location.replace('https://cc-converter-s3723009.ts.r.appspot.com/') </script>";
 }
 
 ?>
@@ -36,15 +74,15 @@ if($_FILES['userfile']['type'] != 'image/jpeg') {
 <body>
     <h1>Cloud Converter</h1>
     <h3>Created by: <br> Ming s3723009 <br> Ty s3668469</h3>
-    <h4>Here is your converted file:</h4>
+    <h4>Converting your file:</h4>
     <div class='userUpload'>
-        <h2> <?php echo $file_name; ?> </h2>
-        <form action="<?php echo $_url; ?>">
-            <input type="submit" value="Download Link" />
-        </form>
-        <form action="/email">
-            <input type="hidden" name="public_url" value="<?php echo $public_url; ?>">
-            <input type="submit" value="Email me!" />
-        </form>
+        <h2> Your file: <?php echo $file_name; ?> will be ready soon: <br> <span id="countdown"></span></h2>
+        <a href="<?php echo $public_url; ?>" download="image">
+            <button type="button" id="download" disabled="disabled">Download</button>
+        </a>
+    </div>
+    <div class="disclaimer">
+        <h2>Disclaimer</h2>
+        <p>All files will be deleted at the start of each week. We are not responsible for lost files.</p>
     </div>
 </body>
